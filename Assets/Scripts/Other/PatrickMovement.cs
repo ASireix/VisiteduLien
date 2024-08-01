4141e2877071ad6d73e23ee19443d9eb5ea3b025
ASireix <tvv3show@gmail.com>
2024-08-01 12:16:13 +0200

    {Update} Main event Prefab updated

--- Assets/Scripts/Other/PatrickMovement.cs
+++ Assets/Scripts/Other/PatrickMovement.cs
@@ -1,28 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class PatrickMovement : MonoBehaviour
 {
     [Header("Positions")]
-    [SerializeField] Vector3 upPosition;
-    [SerializeField] Vector3 downPosition;
-    [SerializeField] Vector3 leftPosition;
-    [SerializeField] Vector3 rightPosition;
-    [SerializeField] Vector3 customPosition;
+    [SerializeField] TransformData upPosition;
+    [SerializeField] TransformData downPosition;
+    [SerializeField] TransformData leftPosition;
+    [SerializeField] TransformData rightPosition;
+    [SerializeField] TransformData customPosition;
 
     [Header("References")]
     [SerializeField] Transform transformRef;
     [SerializeField] float speed = 2f;
 
-    Vector3 startPos;
+    TransformData startPos;
     bool init;
     // Start is called before the first frame update
     void Start()
     {
         if (!init)
         {
-            startPos = transformRef.localPosition;
+            startPos = new TransformData(transformRef.localPosition, transformRef.localEulerAngles);
         }
         init = true;
     }
@@ -47,6 +48,11 @@ public class PatrickMovement : MonoBehaviour
         Move(rightPosition);
     }
 
+    public void MoveCustom()
+    {
+        Move(customPosition);
+    }
+
     public void Move(Vector3 pos, bool tween = true)
     {
         if (tween)
@@ -59,11 +65,37 @@ public class PatrickMovement : MonoBehaviour
         }
     }
 
+    public void Move(TransformData transData, bool tween = true)
+    {
+        if (tween)
+        {
+            LeanTween.moveLocal(transformRef.gameObject, transData.Position, speed);
+            LeanTween.rotateLocal(transformRef.gameObject, transData.Rotation, speed);
+        }
+        else
+        {
+            transformRef.SetLocalPositionAndRotation(transData.Position, Quaternion.Euler(transData.Rotation));
+        }
+    }
+
     private void OnEnable()
     {
         if (init)
         {
             Move(startPos, false);
         }
+    }
+}
+
+[System.Serializable]
+public class TransformData
+{
+    public Vector3 Position;
+    public Vector3 Rotation;
+
+    public TransformData (Vector3 Position, Vector3 Rotation)
+    {
+        this.Position = Position;
+        this.Rotation = Rotation;
     }
 }
